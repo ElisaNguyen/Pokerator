9ec27b2c8ac52e9c1fc6452eddc6583ebc90f8ae
Dominique <dominiquegeissler@gmx.de>
2019-11-26 14:45:34 +0100

    data ordner hiinzugefügt

--- NameEvaluation.py
+++ NameEvaluation.py
@@ -2,3 +2,7 @@
 Likelihood of letters appearing in that order (n-grams of CMU dict or pokedex?)
 → Namelette in “Checking the phonetic likelihood of the new word”
 In case all are bad: pokemon suffixes? (e.g. horsemon)"""
+
+import panda as pd
+
+poke_data = pd.read_csv("")
