828d5e19a798991a5a3752e595407e733f1d8608
Elisa Nguyen <elisa.nguyen@live.de>
2019-11-26 17:55:59 +0100

    draft GUI - still under construction

--- GUI.py
+++ GUI.py
@@ -1,2 +1,23 @@
+#DISCLAIMER: STILL UNDER CONSTRUCTION
+
 #User Interface
+#https://likegeeks.com/python-gui-examples-tkinter-tutorial/
+# from tkinter import *
+#
+# window = Tk()
+# window.geometry('350x200')
+# window.title('Pokérator')
+#
+# def clicked():
+#     lbl.configure(text="Button was clicked !!")
+#
+# lbl = Label(window, text="Pokérator - Unveil your inner Pokémon", font=("Arial Bold", 30))
+# lbl.grid(column=0, row=0)
+#
+# txt = Entry(window,width=10)
+# txt.grid(column=1, row=0)
+#
+# btn = Button(window, text="Submit", command=clicked)
+# btn.grid(column=0, row=1)
+# window.mainloop()
 
