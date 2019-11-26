12fdc669c78a8ba3af89c5235639a657bda6da18
Elisa Nguyen <elisa.nguyen@live.de>
2019-11-26 15:04:43 +0100

    Moved xlsx files into data folder get_questions method in QuestionHandling

--- QuestionHandling.py
+++ QuestionHandling.py
@@ -1,1 +1,12 @@
-#randomly ask more than 2 questions to user to get the input
+"""
+Ask questions to the user and get their responses.
+Questions are stored in APNLP_QuestionsToUser.xlsx
+"""
+import pandas as pd
+
+#method that returns questions to ask to the user in a dictionary format
+def get_questions():
+    data = pd.read_excel(r'Data\APNLP_QuestionsToUser.xlsx')
+    questions = pd.DataFrame(data, columns=['Questions'])
+    return questions
+
