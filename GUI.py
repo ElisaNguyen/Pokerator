71adce93b5b98e4d11132f087f67e2a88a1290fd
Elisa Nguyen <elisa.nguyen@live.de>
2019-11-29 14:22:30 +0100

    draft GUI - still under construction - commented out, missing submit button POKEDEXENTRYBUILDER - started to build the description

--- GUI.py
+++ GUI.py
@@ -1,23 +1,34 @@
-#DISCLAIMER: STILL UNDER CONSTRUCTION
-
-#User Interface
-#https://likegeeks.com/python-gui-examples-tkinter-tutorial/
+# #DISCLAIMER: STILL UNDER CONSTRUCTION
+#
+# #User Interface
+# #https://likegeeks.com/python-gui-examples-tkinter-tutorial/
 # from tkinter import *
 #
-# window = Tk()
-# window.geometry('350x200')
-# window.title('Pokérator')
 #
-# def clicked():
-#     lbl.configure(text="Button was clicked !!")
+# def submit(answers):
+#     for answer in answers:
+#         print(answers.get())
 #
-# lbl = Label(window, text="Pokérator - Unveil your inner Pokémon", font=("Arial Bold", 30))
-# lbl.grid(column=0, row=0)
+# #Method to build the GUI, expects questions in dictionary form as input
+# def build_GUI(questions):
+#     window = Tk()
+#     window.geometry('350x200')
+#     window.title('Pokérator')
 #
-# txt = Entry(window,width=10)
-# txt.grid(column=1, row=0)
+#     lbl = Label(window, text="Pokérator - Unveil your inner Pokémon", font=("Arial Bold", 15))
+#     lbl.grid(column=0, row=0)
 #
-# btn = Button(window, text="Submit", command=clicked)
-# btn.grid(column=0, row=1)
-# window.mainloop()
-
+#     answers = []
+#     for i in range(len(questions)):
+#         qtxt = Label(window, text=str(list(questions.keys())[i]), font=("Arial", 11))
+#         qtxt.grid(column=0, row=10+i)
+#         answer = Entry(window, width=10)
+#         answers.append(answer)
+#         answer.grid(column=2, row=10+i)
+#
+#     btn = Button(window, text="Submit", command=submit(answers))
+#     btn.grid(column=0, row=30)
+#     window.mainloop()
+#
+# qs= {"What?": "a", "Why?": "b"}
+# build_GUI(qs)
--- PokedexEntryBuilder.py
+++ PokedexEntryBuilder.py
@@ -8,13 +8,39 @@ Insert conceptnet response into template
 E.g. Horsemon can jump a barrier.
 Adjust grammar if necessary?
 """
+import pandas as pd
+import requests
+import re
+import numpy as np
 
-def conceptnet_request(word):
-    """
+#Method to import the template for description building based on conceptnet edgetype
+def get_template(edgetype):
+    data = pd.read_excel(r'Data\APNLP_templates.xlsx')
+    df = pd.DataFrame(data, columns = ['Edge types', 'Template'])
+    templates = df.values.tolist()
+    for t in templates:
+        if t[0] == edgetype:
+            return t[1]
 
-    :param word:
-    :return:
-    """
-    #random from list of relations
-    #api call
-    pass
+#Method for requesting the word and relation(edgetype) from conceptnet.
+#To be outsourced to new tool-file
+def conceptnet_request(word, relation):
+    url = 'http://api.conceptnet.io/c/en/' + word + '?rel=/r/' + relation + '&limit=10'
+    print(url)
+    response = requests.get(url).json()
+    df = pd.DataFrame(response['edges'])
+    surface_texts = list(df[df['rel'].apply(lambda e: dict(e)['label'] == relation)]['surfaceText'])
+    words = [e.replace('[', '').replace(']', '') for e in re.findall("\[+[a-z A-Z]+\]+", str(surface_texts))]
+    words = list(set(words))
+    if word in words:
+        words.remove(word)
+    return surface_texts, words
+
+#Method to write the description of the Pokemon, randomly select 2 sentences
+def build_description(answers):
+    #random generator in range of len(answers) to take the random answer to base the description on
+    #afterwards delete that answer from the answers list
+    #do the random thing again
+    #build the description
+    description = "test"
+    return description
