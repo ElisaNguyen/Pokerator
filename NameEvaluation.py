b68298cd94bf8c36a4a7a1b121b503fdc5e9761d
Dominique <dominiquegeissler@gmx.de>
2019-11-29 14:21:15 +0100

    name evaluation änderung zu CMU anstatt pokemon Datensat

--- NameEvaluation.py
+++ NameEvaluation.py
@@ -2,18 +2,27 @@
 Likelihood of letters appearing in that order (n-grams of CMU dict or pokedex?)
 → Namelette in “Checking the phonetic likelihood of the new word”
 In case all are bad: pokemon suffixes? (e.g. horsemon)"""
-
+import nltk
 import pandas as pd
+import nltk.tokenize.sonority_sequencing as sequencing
 from nltk.util import ngrams
 import math
+import re
+nltk.download('cmudict')
 
 
-def load_data():
+def load_poke_data():
     # load the pokemon data with all the names
     poke_data = pd.read_csv("Data/pokemon.csv")
     poke_df = pd.DataFrame(poke_data)
     return poke_df
 
+def load_data():
+    entries = nltk.corpus.cmudict.entries()
+    data = []
+    for item in entries:
+        word, pronunciation = item
+        data.extend(word for word in re.findall("[a-z]{3,}", word))
 
 def ngram_lists():
     # create a n-gram list with the n-grams from all the pokemon names
@@ -22,11 +31,12 @@ def ngram_lists():
     poke_unigram_list = []
     for name in df['name']:
         characters = list(name.lower())
+        # tok = sequencing.SyllableTokenizer()
         n_grams = list(ngrams(characters, 2))
         poke_bigram_list.extend(n_grams)
         poke_unigram_list.extend(characters)
 
-    V = len(poke_unigram_list)
+    V = len(poke_bigram_list)
     return poke_bigram_list, poke_unigram_list, V
 
 
@@ -79,7 +89,8 @@ def run():
     unigram_vocab, unigram_count = frequency_count(unigram_list)
 
     bigram_probabilities = probability_list(bigram_vocab, bigram_count, unigram_count, V)
-    likelihood = evaluation("Barkerator", bigram_probabilities, unigram_count, V)
+    likelihood = evaluation("Pokemon", bigram_probabilities, unigram_count, V)
+    likelihood = 2**likelihood
     print(likelihood)
     return likelihood
 
