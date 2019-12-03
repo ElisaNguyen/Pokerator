b6cac6dee4dfb009d8d9a98c186f60868d60753b
Elisa Nguyen <elisa.nguyen@live.de>
2019-12-03 17:56:34 +0100

    Updated templates Descriptions are working

--- PokedexEntryBuilder.py
+++ PokedexEntryBuilder.py
@@ -9,38 +9,60 @@ E.g. Horsemon can jump a barrier.
 Adjust grammar if necessary?
 """
 import pandas as pd
-import requests
-import re
-import numpy as np
+import random
+import ConceptNet as cn
 
-#Method to import the template for description building based on conceptnet edgetype
 def get_template(edgetype):
-    data = pd.read_excel(r'Data\APNLP_templates.xlsx')
+    """
+    Method to import the template for description building based on conceptnet edgetype
+
+    :param edgetype: a string that is one of the conceptNet edgetypes
+    :return: template for the edgetype that is specified in the excel
+    """
+    data = pd.read_excel(r'C:\Users\Elisa\PycharmProjects\Pokerator\Data\APNLP_templates.xlsx')
     df = pd.DataFrame(data, columns = ['Edge types', 'Template'])
     templates = df.values.tolist()
     for t in templates:
         if t[0] == edgetype:
             return t[1]
 
-#Method for requesting the word and relation(edgetype) from conceptnet.
-#To be outsourced to new tool-file
-def conceptnet_request(word, relation):
-    url = 'http://api.conceptnet.io/c/en/' + word + '?rel=/r/' + relation + '&limit=10'
-    print(url)
-    response = requests.get(url).json()
-    df = pd.DataFrame(response['edges'])
-    surface_texts = list(df[df['rel'].apply(lambda e: dict(e)['label'] == relation)]['surfaceText'])
-    words = [e.replace('[', '').replace(']', '') for e in re.findall("\[+[a-z A-Z]+\]+", str(surface_texts))]
-    words = list(set(words))
-    if word in words:
-        words.remove(word)
-    return surface_texts, words
+def get_random_edgetype():
+    """
+    Method to get random edgetype
 
-#Method to write the description of the Pokemon, randomly select 2 sentences
+    :return: a random edgetype as a string
+    """
+    data = pd.read_excel(r'C:\Users\Elisa\PycharmProjects\Pokerator\Data\APNLP_templates.xlsx')
+    df = pd.DataFrame(data, columns = ['Edge types'])
+    edgetypes = df.values.tolist()
+    return str(random.choice(edgetypes))[2:-2]
+
+def build_sentence(word, relation):
+    """
+    Method to write a sentence given a word and a relation
+
+    :param word: a word which was one of the answers given by the user
+    :param relation: a relation (same as edgetype) from conceptnet
+    :return: a sentence as string
+    """
+    t = str(get_template(relation))
+    r = cn.conceptnet_request(word, relation)
+    if len(r[1]) == 0:
+        r = cn.conceptnet_request(word, "AtLocation")
+    sentence = t.replace(t[t.find("<"):t.find(">")+1], str(r[1][0])) + ". "
+    return sentence
+
 def build_description(answers):
-    #random generator in range of len(answers) to take the random answer to base the description on
-    #afterwards delete that answer from the answers list
-    #do the random thing again
-    #build the description
-    description = "test"
+    """
+    Method to write the description of the Pokemon, randomly select 2 sentences
+
+    :param answers: list of answers given by user
+    :return: a description as string
+    """
+    description = ""
+    for i in range(2):
+        answer = random.choice(answers)
+        answers.remove(answer)
+        relation = get_random_edgetype()
+        description = description + build_sentence(answer, relation)
     return description
