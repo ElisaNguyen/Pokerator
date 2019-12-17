e9f91cb05d8e84fcc033faa4767a4907506916cf
Elisa Nguyen <elisa.nguyen@live.de>
2019-12-17 21:32:27 +0100

    updated templates with expected pos tag updated conceptnet request to search for fitting relation itself pokedexentrybuilder now making descriptions fitting to pos tagging based on dependency parsing

--- ConceptNet.py
+++ ConceptNet.py
@@ -1,25 +1,41 @@
 import requests
 import pandas as pd
 import re
+import random
 
+def get_all_relations():
+    """
+    Method to return all relations of conceptnet specified in the xlsx
 
-def conceptnet_request(word, relation):
+    :return: List of all relations, as strings
+    """
+    data = pd.read_excel(r'C:\Users\Elisa\PycharmProjects\Pokerator\Data\APNLP_templates.xlsx')
+    df = pd.DataFrame(data, columns = ['Edge types'])
+    edgetypes = df.values.tolist()
+    relations = []
+    for e in edgetypes:
+        relations.append(str(e)[2:-2])
+    return relations
+
+def conceptnet_request(word):
     """
     Send an API request to conceptNet for a word and for a certain relation with the word the surface text in which it
     can be used and the related words.
 
     :param word: a word
-    :param relation: a relation of conceptNet
-    :return: list of surface texts for the word and the relation and a list of words related to the input word
+    :return: list of words to a given relation, that relation, and the surface texts from conceptnet
     """
     url = 'http://api.conceptnet.io/c/en/' + word + '?limit=100'
     print(url)
     response = requests.get(url).json()
     df = pd.DataFrame(response['edges'])
-    surface_texts = list(df[df['rel'].apply(lambda e: dict(e)['label'] == relation)]['surfaceText'])
+    unique_edges = df['rel'].apply(lambda e: dict(e)['label']).unique()
+    possible_edges = [value for value in unique_edges if value in get_all_relations()]
+    chosen_edge = random.choice(possible_edges)
+    surface_texts = list(df[df['rel'].apply(lambda e: dict(e)['label'] == chosen_edge)]['surfaceText'])
     words = [e.replace('[', '').replace(']', '') for e in re.findall("\[+[a-z A-Z]+\]+", str(surface_texts))]
     words = [w.lower().replace('the ', '').replace('a ', '') for w in words]
     words = list(set(words))
     if word in words:
         words.remove(word)
-    return surface_texts, words
+    return words, chosen_edge, surface_texts
