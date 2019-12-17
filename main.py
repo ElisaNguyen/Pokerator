10fce640cf208e9a8ade812758cd9044a520e4cf
Daphne <daphne.theo@yahoo.de>
2019-12-17 20:06:00 +0100

    merge everything part 3

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from NameEvaluation import run
 from PokedexEntryBuilder import build_description
 from QuestionHandling import ask_questions, select_answers
 from Wordblender import blend_a_word
-import pandas as pd
 
 if __name__== "__main__":
+    # evaluate two longest words
     # answers = ask_questions()
     # w1, w2 = select_answers(list(answers.values()))
     # blended_words = blend_a_word(w1, w2)
@@ -13,16 +13,17 @@ if __name__== "__main__":
     # print(pokemon)
     # print(description)
 
-    #answers = ask_questions()
-    answers=['Daphne', 'June', 'dog', 'mouse', 'cat', 'tree']
+    # evaluate all answers
+    answers = ask_questions()
     blended_words = dict()
     for w1 in answers:
         for w2 in answers:
             if w1 != w2:
                 words = blend_a_word(w1, w2)
                 for w in words:
-                    blended_words[w] = [w1,w2]
+                    blended_words[w] = [w1, w2]
     pokemon = run(blended_words.keys())
     description = build_description(blended_words[pokemon])
+    print('Your Pokémon: ')
     print(pokemon)
     print(description)
