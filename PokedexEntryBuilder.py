3e7505f9ff860ca0f7c75ae4683490cb68783d21
Daphne <daphne.theo@yahoo.de>
2020-01-10 16:16:39 +0100

    Random choice of relation (conceptnet)

--- PokedexEntryBuilder.py
+++ PokedexEntryBuilder.py
@@ -15,6 +15,7 @@ import spacy
 from spellchecker import SpellChecker
 import gpt_2_simple as gpt2
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['KMP_WARNINGS'] = 'off'
 
@@ -32,6 +33,7 @@ def get_template(edgetype):
     chosen_t = random.choice(templates)
     return chosen_t[0], chosen_t[1]
 
+
 def get_random_edgetype():
     """
     Method to get random edgetype
@@ -40,14 +42,15 @@ def get_random_edgetype():
     """
     return str(random.choice(cn.get_all_relations()))
 
+
 def filter_word_pos(word_list, pos_list):
-    '''
+    """
     Method to choose one word/characteristic out of the word list received from conceptnet
     Based on POS tagging of the root of the sentence (dependency parsing) and the expected POS tag
     :param word_list: word list from conceptnet
     :param pos_list: from excel, expected pos for the requested sentence in list form
     :return: list of characteristics out of word list that fits to the expected pos tag
-    '''
+    """
     word_list_possible = []
     nlp = spacy.load('en_core_web_sm')
     for characteristic in word_list:
@@ -59,18 +62,20 @@ def filter_word_pos(word_list, pos_list):
                         word_list_possible.append(characteristic)
     return word_list_possible
 
+
 def check_spelling(word):
-    '''
+    """
     Method to check whether a word is spelled correctly and returns the most likely word if its mispelled based on the minimum edit distance
     :param word: a word (answer by user)
     :return: correctly spelled word
-    '''
+    """
     spell = SpellChecker()
     mispelled = spell.unknown([word])
     if len(mispelled) > 0:
         return spell.correction(word)
     else:
         return word
+
 
 def build_sentence(word):
     """
@@ -80,14 +85,17 @@ def build_sentence(word):
     :return: a sentence as string
     """
     cn_answer = cn.conceptnet_request(check_spelling(word))
-    for edge in cn_answer[1]:
+    edges = cn_answer[1]
+    random.shuffle(edges)
+    for edge in edges:
         t = str(get_template(edge)[0])
         word_list = cn_answer[0][edge]
         possible_words = filter_word_pos(word_list, str(get_template(edge)[1]).split(','))
         if len(possible_words) > 0:
             chosen_word = random.choice(possible_words)
-            sentence = t.replace(t[t.find("<"):t.find(">")+1], chosen_word) + ". "
+            sentence = t.replace(t[t.find("<"):t.find(">") + 1], chosen_word) + ". "
             return sentence
+
 
 def build_description(answers, name):
     """
@@ -137,8 +145,7 @@ def generation_gtp2(input_sent, name):
         gpt2.load_gpt2(sess, run_name='run3')
     desc = gpt2.generate(sess, run_name='run3', length=30, prefix=input_sent, return_as_list=True)[0]
     desc = filter_pokemon_names(desc, name)
-    desc = desc.replace('. ','.')
+    desc = desc.replace('. ', '.')
     desc = '.\n'.join(desc.split('.')[:-1]) + '.'
-    desc = desc.replace('  ',' ').replace('\n ', '\n')
+    desc = desc.replace('  ', ' ').replace('\n ', '\n')
     return desc
-
